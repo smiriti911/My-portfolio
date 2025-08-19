b753e91efa81aadba85e40ef7adf6cf8e76b68f2
Smiriti Kapoor <smiritikapoor911@gmail.com>
2025-08-19 09:12:32 +0530

    fixes

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
 
    const [copied, setCopied] = useState(false);
   const Email = {
-    name: "SmiritiKapoor",
+    name: "Smiriti Kapoor",
     email: "smiritikapoor911@gmail.com"
   }
 
@@ -149,7 +149,7 @@ border border-neutral-800 "
 {/* Get in Touch */}
       <div className=" p-4 rounded-2xl flex flex-col gap-6 bg-neutral-900/50 border-1 border-neutral-800">
         <h2   className={`text-transparent bg-clip-text bg-gradient-to-b mt-4 from-neutral-100 to-neutral-500 text-xl sm:text-2xl font-light tracking-wide ${redRose.className}`}>Get in Touch</h2>
-        <ul className="flex flex-col gap-4 md:gap-6 text-lg mt-4 md:mt-10">
+        <ul className="flex flex-col gap-4 md:gap-6 text-lg mt-3 sm:mt-4 md:mt-10">
           <li>
             <a
               href="https://www.linkedin.com/in/smiriti-kapoor011/"
@@ -182,7 +182,7 @@ border border-neutral-800 "
 
           <li>
 
-              <div className="flex items-center justify-between gap-3 w-full group cursor-pointer">
+              <div className="flex items-center justify-between w-full group cursor-pointer">
   {/* Email with icon */}
   <div className="flex items-center gap-3">
     <MdEmail className="text-2xl text-lime-300" />
