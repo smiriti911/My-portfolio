ff3cb35443996637952ce9c1417cec9a85ece75c
Smiriti Kapoor <smiritikapoor911@gmail.com>
2025-08-24 13:28:24 +0530

    m

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -57,13 +57,12 @@ const Contact = () => {
     setLoading(true);
     setSuccess(false);
 
-    emailjs
-      .sendForm(
-        "service_0uuctnk",
-        "template_xtxoc3m",
-        formRef.current,
-        "6Af6qneRkYaxiIDaj"
-      )
+   emailjs.sendForm(
+  process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+  process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+  formRef.current,
+  process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+)
       .then(
         (result) => {
           setLoading(false);
